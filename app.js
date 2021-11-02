a72241372dd579b07c2a2bd6b955ac7f6703db35
Matias Miguez <matiaspakua@gmail.com>
2021-11-01 23:10:13 -0300

    Se cambia la URL al deploy de heroku

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 document.addEventListener('DOMContentLoaded', () => {
 
-  const URI = 'https://calm-lake-70360.herokuapp.com';
+  const URI = 'https://psa-api-pagos.herokuapp.com/';
   
   // materialize setup
   M.AutoInit();
